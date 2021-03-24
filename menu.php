c479aabdfd38ad6c4ff68bdccdbec9d1beede1f3
Louis Chen <chenloui@usc.edu>
2021-03-24 00:17:16 -0400

    new files

---
+++ menu.php
@@ -0,0 +1,20 @@
+<nav class="navbar navbar-expand-md bg-light navbar-light">
+
+  <a class="navbar-brand" href="home.php"><h3><strong>Ride the Beat</strong></h3></a>
+
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item"><a class="nav-link" href="about.php">About</a></li>
+      <li class="nav-item"><a class="nav-link" href="dashboard.php">Dashboard</a></li>
+    </ul>
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item"><a class="nav-link" href="login.php">Login/Logout</a></li>
+      <li class="nav-item"><a class="nav-link" href="signup.php">Sign Up</a></li>
+      <li class="nav-item"><a class="nav-link" href="profile.php">My Profile</a></li>
+    </ul>
+  </div>
+</nav>
