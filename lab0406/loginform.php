0e96d772efa420a776647c297037f6ee2ef7fc6f
Louis Chen <chenloui@usc.edu>
2021-04-03 12:44:32 -0400

    lab files

---
+++ lab0406/loginform.php
@@ -0,0 +1,12 @@
+<form action="" method="POST">
+    <br>
+    The password is "password"
+    <br>
+    <br>
+    Password: <input type="text" name="password">
+    <br>
+    <br>
+
+    <input type="submit">
+
+</form>
