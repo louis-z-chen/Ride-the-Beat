5d5cc5f8cd425e3f24cc608e3f36b63b8bd16ee1
sambudiartho <budiarth@usc.edu>
2021-04-03 19:21:57 -0700

    update home.php

--- home.php
+++ home.php
@@ -14,6 +14,10 @@
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	<link href= "styles.css" rel="stylesheet">
 
+
+<style>
+
+</style>
 </head>
 <body>
 	<?php require "menu.php"; ?>
