07709e0fa3c1c924c013c0272852dd431d49f529
sambudiartho <budiarth@usc.edu>
2021-04-03 19:21:32 -0700

    Welcome page

---
+++ welcome/welcome.php
@@ -0,0 +1,122 @@
+<html>
+<head>
+  <link rel="stylesheet" href="https://use.typekit.net/vps8gbz.css">
+	<title>Ride the Beat</title>
+
+<style>
+
+body {
+
+background: linear-gradient(-45deg, #1DB954, #000000, #000000);
+background-size: 400% 400%;
+animation: gradient 6s ease infinite;
+}
+
+@keyframes gradient {
+     0% {
+         background-position: 10% 50%;
+     }
+
+     50% {
+         background-position: 65% 50%;
+     }
+
+     100% {
+         background-position: 0% 50%;
+     }
+
+ }
+
+ #maintext {
+   font-family: rubik, sans-serif;
+   font-weight: 400;
+   font-size: 20px;
+   font-style: normal;
+   width: 600px;
+   margin-left: auto;
+   margin-right: auto;
+   margin-top: 50px;
+   text-align: center;
+   color: #B1E8C7;
+   line-height: 30px;
+
+ }
+
+ #logo {
+   width: 200px;
+   margin-left: 50px;
+   margin-top: 30px;
+   float: left;
+
+ }
+
+ .logo {
+   width: 180px;
+   position: relative;
+ }
+
+#ridethebeattext {
+  width: 600px;
+  margin-left: auto;
+  margin-right: auto;
+  padding-top: 80px;
+}
+
+.ridethebeattext {
+  width: 600px;
+  position: relative;
+}
+
+ #loginbutton {
+   font-family: rubik, sans-serif;
+   font-weight: 900;
+   font-style: normal;
+   font-size: 25px;
+   width: 300px;
+   height: 70px;
+   display: block;
+   text-align: center;
+   line-height: 50px;
+   background-color: #B1E8C7;
+   color: #017100;
+   border: 5px solid;
+   margin-top: 70px;
+   margin-left: auto;
+   margin-right: auto;
+   padding-top: 25px;
+
+ }
+
+ #loginbutton:hover {
+   background-color: #017100;
+   color: #B1E8C7;
+   transition: all 0.5s ease-in-out;
+ }
+
+
+</style>
+</head>
+<body>
+  <div id="logo">
+     <img src="logo3.png" class="logo">
+
+  </div> <!--div for logo-->
+
+<div id="ridethebeattext">
+   <img src="ridethebeattext.png" class="ridethebeattext">
+
+</div> <!--div for ridethebeattext-->
+
+<div id="maintext">
+  <p>
+    We act as a platform for you to create, manage, and <br /> share playlists.
+    Ride the beat and interact with music <br /> uniquely and to discover emerging artists.
+  </p>
+
+</div> <!--div for maintext-->
+
+<div id="loginbutton"
+  <h1>DISCOVER NOW</h1>
+
+</body>
+</html>
