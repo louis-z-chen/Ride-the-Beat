204ddf5c9da4475be88d1f5729707514baab0668
Louis Chen <chenloui@usc.edu>
2021-04-05 12:43:39 -0400

    navbar function done

--- menu.php
+++ menu.php
@@ -1,30 +1,38 @@
-<nav class="navbar navbar-expand-md bg-light navbar-light">
+<nav class="navbar navbar-expand-lg bg-dark navbar-dark sticky-top">
 
-  <a class="navbar-brand" href="home.php"><h3><strong>Ride the Beat</strong></h3></a>
+  <div class = "container-fluid">
 
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+    <a class="navbar-brand" href="#">
+      <img src="images/Logo3.png" height="80" class="d-inline-block pull-left" alt="">
+      <span class="name pull-left"><strong>Ride the Beat</strong></span>
 
-  <div class="collapse navbar-collapse" id="collapsibleNavbar">
-    <div class="navbar-nav mr-auto">
-      <form class="navbar-form navbar-left" action="results.php">
-        <div class="input-group">
-          <input type="text" class="form-control" placeholder="Search" name="search">
-          <div class="input-group-btn">
-            <button class="btn btn-success" type="submit">
-              <i class="fas fa-search"></i>
-            </button>
+    </a>
+
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+      <div class="navbar-nav mr-auto">
+        <form class="navbar-form navbar-left" action="results.php">
+          <div class="input-group">
+            <input type="text" class="form-control" placeholder="Search" name="search">
+            <div class="input-group-btn">
+              <button class="btn btn-success" type="submit">
+                <i class="fas fa-search"></i>
+              </button>
+            </div>
           </div>
-        </div>
-      </form>
+        </form>
+      </div>
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item"><a class="nav-link" href="">About</a></li>
+        <li class="nav-item"><a class="nav-link" href="">Discover</a></li>
+        <li class="nav-item"><a class="nav-link" href="">Playlists</a></li>
+        <li class="nav-item"><a class="nav-link" href="">Profile</a></li>
+        <li class="nav-item"><a class="nav-link btn btn-success navbar-btn green-button" href="logout.php">Log Out</a>
+        </li>
+      </ul>
     </div>
-    <ul class="navbar-nav ml-auto">
-      <li class="nav-item"><a class="nav-link" href="">About</a></li>
-      <li class="nav-item"><a class="nav-link" href="">Discover</a></li>
-      <li class="nav-item"><a class="nav-link" href="">Playlists</a></li>
-      <li class="nav-item"><a class="nav-link" href="">Profile</a></li>
-      <li class="nav-item"><a class="nav-link" href="logout.php">Log Out</a></li>
-    </ul>
   </div>
 </nav>
