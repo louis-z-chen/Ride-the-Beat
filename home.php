87cb9cd73f05d9804857fcc0efc2e448449839cc
sambudiartho <budiarth@usc.edu>
2021-04-05 16:52:56 -0700

    home update & stylesheet

--- home.php
+++ home.php
@@ -28,9 +28,10 @@
 		display: flex;
 		padding: 20px;
 		flex-basis: : 80%;
-		justify-content: space-between;
+		justify-content: center;
   	align-items: center;
 		margin: auto;
+		margin-left: 50px;
 
 	}
 	.main-container div{
@@ -44,7 +45,7 @@
 		flex-basis: 80%;
 		flex-wrap: wrap;
 		flex: 0 0 65%;
-		height: 700px;
+		height: 760px;
 
 	}
 	.right-box {
@@ -53,7 +54,7 @@
 		flex-basis: 50%;
 		flex: 0 0 10%
 		flex-wrap: wrap;
-		height: 700px;
+		height: 760px;
 
 	}
 	.left-banner {
@@ -70,13 +71,14 @@
 	.box-4{
 		flex: 2;
 		flex-basis: auto;
-
-
+		color: #FFFFFF
 	}
+
 	.mainalbum{
 		display: block;
-		height: 200px;
+		height: 230px;
 		margin: auto;
+		margin-top: 30px;
 		border-radius: 15px;
 
 	}
@@ -87,20 +89,20 @@
 		color: #FFFFFF;
 		font-size: 16px;
 		font-family: rubik;
-		border: 1px solid red;
+	/*	border: 1px solid red; */
 		flex-direction: row;
 		justify-content: space-evenly;
 
 	}
 
-	.box-artist{
+	.artist-information{
 		display: flex;
 		margin: 20px;
 		flex-basis: auto;
 		color: #FFFFFF;
 		font-size: 12px;
 		font-family: rubik;
-		border: 1px solid red;
+	/*	border: 1px solid red; */
 		flex-direction: row;
 		justify-content: space-evenly;
 
@@ -113,13 +115,13 @@
 		color: #FFFFFF;
 		font-size: 16px;
 		font-family: rubik;
-		border: 1px solid yellow;
+	/*	border: 1px solid yellow; */
 		justify-content: space-evenly;
 
 	}
 	.album-1{
 		display: flex;
-		border: 1px solid green;
+/*		border: 1px solid green; */
 		text-align: justify;
 		font-size: 12px;
 		color: #FFFFFF;
@@ -142,6 +144,7 @@
 		margin:auto;
 		border-radius: 3px;
 	}
+
 	.spotlight{
 		display: block;
 		height: 200px;
@@ -153,18 +156,56 @@
 	.songName{
 		font-size: 20px;
 		color: #FFFFFF;
+		text-align: center;
 	}
 	.artistName{
 		color: #B3B3B3;
 		font-size: 16px;
+		text-align: center;
 	}
 
+	.musicbar {
+		display: block;
+		width: 300px;
+		margin: auto;
+		margin-top: 10px;
+		margin-bottom: 20px;
 
-	.spotlight {
+	}
+
+	.lyrics {
+		display: block;
+		height: 200px;
+		margin: auto;
+		margin-top: 20px;
+		font-size: 14px;
 		flex: 2;
 		flex-basis: auto;
+		color: #b3b3b3;
+		overflow-y: auto;
+		line-height: 20px;
+		padding-left: 10px;
+		padding-bottom: 10px;
+
 	}
 
+.lyrics::-webkit-scrollbar {
+    -webkit-appearance: none;
+}
+
+.lyrics::-webkit-scrollbar:vertical {
+    width: 11px;
+}
+
+.lyrics::-webkit-scrollbar:horizontal {
+    height: 11px;
+}
+
+.lyrics::-webkit-scrollbar-thumb {
+    border-radius: 8px;
+    border: 2px solid white; /* should match background, can't be transparent */
+    background-color: white;
+}
 
 
 </style>
@@ -174,7 +215,6 @@
 
 <div class="main-container">
   <div class="left-box">
-		<h3>Left Box</h3>
 		  <div class="left-banner">
 				<h6>//TRENDING</h6>
 				<h4>Free Spirit</h4>
@@ -203,8 +243,35 @@
 				<div class="album-1">
 					<img src="images/Darci.jpg" class="allalbums">
 				</div>
-			</div><!-- close box-5 -->
 
+			</div><!-- close underrated artists-->
+
+			<div class="artist-information">
+				<p>
+					Jake Hope <br /> <span style="font-size: 10px">97,406 monthly listeners </span>
+				</p>
+
+				<p>
+					Kyle Lux <br /> <span style="font-size: 10px">223,964 monthly listeners </span>
+				</p>
+
+				<p>
+					Always Never <br /> <span style="font-size: 10px">560,446 monthly listeners </span>
+				</p>
+
+				<p>
+					All Day <br /> <span style="font-size: 10px"> 462,595 monthly listeners </span>
+				</p>
+
+				<p>
+					Luke Chiang <br /> <span style="font-size: 10px"> 453,110 monthly listeners </span>
+				</p>
+
+				<p>
+					Darci <br /> <span style="font-size: 10px"> 415,886 monthly listeners </span>
+				</p>
+
+			</div> <!--end of artist-information-->
 
 
 			<div class="title">Highest Rated Playlists</div>
@@ -234,19 +301,101 @@
 
 			</div>
 
+			<div class="artist-information">
+				<p>
+					Unplugged <br /> <span style="font-size: 10px">Louis Chen </span>
+				</p>
+
+				<p>
+					Mood Booster <br /> <span style="font-size: 10px">Menna Elamroussy </span>
+				</p>
+
+				<p>
+					Lo-Fi Beats <br /> <span style="font-size: 10px">Sam Budiartho </span>
+				</p>
+
+				<p>
+					Mellow Beats <br /> <span style="font-size: 10px"> Yuji Itadori </span>
+				</p>
+
+				<p>
+					Acoustic Spring <br /> <span style="font-size: 10px"> John Smith </span>
+				</p>
+
+				<p>
+					Chill Vibes <br /> <span style="font-size: 10px"> Elon Musk </span>
+				</p>
+
+			</div> <!--end of artist information for second row-->
+
 
 	</div> <!-- close left box -->
-	<div class="right-box">right box
-			<div class="box-4">Box four
+	<div class="right-box">
+			<div class="box-4">
 				<img src="images/jake_hope.jpeg" class="mainalbum">
 			</div>
 			<div class="spotlight">
 				<div class=songName>Why</div>
 				<div class=artistName>Jake Hope</div>
-			</div> <!--close right box-->
+				<div class="box-4">
+					<img src="images/musicplay.png" class="musicbar"/>
+				</div>
+				<div class="box-4">
+					<h5 style="padding-left: 10px;">Lyrics</h5>
+				<div class="lyrics">
+					<p>
+Spoke my mind but you stayed silent <br />
+No reply auto pilot <br />
+How could you cross me girl you aint even christian <br />
+This shit was toxic why do I miss it? <br />
+Tell me why  <br />
+This life in 3d and you show just one side  <br />
+Numb to my affection girl you paralyzed  <br />
+Keep it all inside  <br />
+Things change feelings fade and I've been on my own since then  <br />
+Without you it feels  <br />
+So strange so vacant but it aint worth the pain again  <br />
+Already broke my heart like one time  <br />
+Two times  <br />
+Im loosing count  <br />
+Oh my  <br />
+You love putting walls up  <br />
+Tear 'em all down  <br />
+Nike A1s  <br />
+Wear 'em all down  <br />
+Flashback to the summer takin trips in my Toyota  <br />
+Just bout drove off of the rockies we both though this life was over  <br />
+You broke me into pieces  <br />
+Still cant patch 'em up  <br />
+You'd send me hearts and peaches  <br />
+These days i pass 'em up  <br />
+I cant  <br />
+I cant seem to give you up  <br />
+Im way past giving up  <br />
+So shawty tell me why  <br />
+This life in 3d and you show just one side  <br />
+Numb to my affection girl you paralyzed  <br />
+Tell me was it all a lie  <br />
+All a lie  <br />
+All a lie  <br />
+Ooooo  <br />
+Things change feelings fade and ive been on my own since then  <br />
+Without you it feels  <br />
+So strange so vacant but it aint worth the pain again  <br />
+Already broke my heart like one time  <br />
+Two times  <br />
+Im loosing count  <br />
+Oh my <br />
+
+					</p>
+				</div>
+
+				</div>
+
+			</div>
 
 
-	</div>
+	</div> <!--close right box-->
 </div> <!-- close main container -->
 
 
