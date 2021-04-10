914645d0ff8de58e91115728c52f0531df324d40
Louis Chen <chenloui@usc.edu>
2021-04-09 23:17:28 -0400

    header and footer files

--- pages/results.php
+++ pages/results.php
@@ -20,24 +20,6 @@ $user_count = 0;
 require "../reusable_code/database_connection.php";
 
 //Song column
-/*
-$sql_song = "SELECT * FROM song_artists_view WHERE song_name = ?;";
-$statement_song = $mysqli->prepare($sql_song);
-$statement_song->bind_param("s", $search);
-$executed = $statement_song->execute();
-
-if(!$executed) {
-    echo $mysqli->error;
-}
-
-$result = $statement_song->get_result();
-$song_row = $result -> fetch_assoc();
-
-$song_count = $result->num_rows;
-echo $song_count;
-
-$statement_song->close();
-*/
 $sql_song = "SELECT * FROM song WHERE name LIKE '%" . $search . "%' ORDER BY name;";
 
 $results_song = $mysqli->query($sql_song);
@@ -91,14 +73,7 @@ $mysqli->close();
 <head>
 	<title>Ride the Beat</title>
 
-	<!-- Required meta tags -->
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-	<script src="https://kit.fontawesome.com/aef7737b1c.js" crossorigin="anonymous"></script>
-  <link rel="preconnect" href="https://fonts.gstatic.com">
-	<link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300&display=swap" rel="stylesheet">
-	<link href= "styles.css" rel="stylesheet">
+	<?php require "../reusable_code/header_files.php"; ?>
 
 </head>
 <body>
@@ -220,12 +195,7 @@ $mysqli->close();
 
 	</div>
 
-
-
-	<!-- Required meta tags -->
-	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+	<?php require "../reusable_code/footer_files.php"; ?>
 
 </body>
 </html>
---
+++ reusable_code/header_files.php
@@ -0,0 +1,16 @@
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+<!-- Fontawesome icons -->
+<script src="https://kit.fontawesome.com/aef7737b1c.js" crossorigin="anonymous"></script>
+
+<!-- Rubik Font -->
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300&display=swap" rel="stylesheet">
+
+<!-- External Stylesheet -->
+<link href= "styles.css" rel="stylesheet">
