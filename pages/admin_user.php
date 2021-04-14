476a100316619cc2649801621dbade73e42acc0a
Louis Chen <chenloui@usc.edu>
2021-04-14 19:17:02 -0400

    admin_user

--- pages/admin_user.php
+++ pages/admin_user.php
@@ -2,7 +2,7 @@
 require "../reusable_code/login_logic.php";
 
 // if not an admin
-if($_SESSION["security_level"] > 2){
+if($_SESSION["security_level"] < 2){
   header('Location: ../pages/home.php');
   exit();
 }
@@ -12,33 +12,33 @@ require "../reusable_code/database_connection.php";
 
 //search
 //declare variables
-$first_name = "";
-$last_name = "";
+$first_name = "%%";
+$last_name = "%%";
 $username = "%%";
-$email = "";
-$security_level = "";
+$email = "%%";
+$security_level = "%%";
 
 //set variables
 if(isset($_REQUEST["first_name"]) && $_REQUEST["first_name"]!=""){
-	$first_name = $_REQUEST["first_name"];
+	$first_name = "%".$_REQUEST["first_name"]."%";
 }
 if(isset($_REQUEST["last_name"]) && $_REQUEST["last_name"]!=""){
-	$last_name = $_REQUEST["last_name"];
+	$last_name = "%".$_REQUEST["last_name"]."%";
 }
 if(isset($_REQUEST["username"]) && $_REQUEST["username"]!=""){
 	$username = "%".$_REQUEST["username"]."%";
 }
 if(isset($_REQUEST["email"]) && $_REQUEST["email"]!=""){
-	$email = $_REQUEST["email"];
+	$email = "%".$_REQUEST["email"]."%";
 }
-if(isset($_REQUEST["security_level"]) && $_REQUEST["security_level"]!=""){
-	$security_level = $_REQUEST["security_level"];
+if(isset($_REQUEST["security_level"]) && $_REQUEST["security_level"]!="" && $_REQUEST["security_level"]!="all"){
+	$security_level = "%".$_REQUEST["security_level"]."%";
 }
 
 //search prepared statement
-$sql_search = "SELECT * FROM users WHERE username LIKE ? ORDER BY username;";
+$sql_search = "SELECT * FROM users WHERE first_name LIKE ? AND last_name LIKE ? AND username LIKE ? AND email LIKE ? AND security_level LIKE ? ORDER BY first_name;";
 $statement_search = $mysqli->prepare($sql_search);
-$statement_search->bind_param('s',$username);
+$statement_search->bind_param('sssss', $first_name, $last_name, $username, $email, $security_level);
 $executed_search = $statement_search->execute();
 
 if(!$executed_search) {
@@ -103,7 +103,12 @@ $mysqli->close();
 							<div class="col-sm-2">
 								<div class="form-group">
 									<label>Security Level</label>
-									<input type="text" name="security_level" class="form-control" value="<?php echo isset($_REQUEST['security_level'])?$_REQUEST['security_level']:''?>" placeholder="Enter security level">
+                  <select class="form-control" name="security_level" placeholder="Enter security level">
+                    <option value = "all" <?php echo $_REQUEST['security_level'] == "all" ? "selected" : "" ?>>All</option>
+                    <option value = "1" <?php echo $_REQUEST['security_level'] == "1" ? "selected" : "" ?>>User</option>
+                    <option value = "2" <?php echo $_REQUEST['security_level'] == "2" ? "selected" : "" ?>>Administrator</option>
+                    <option value = "3" <?php echo $_REQUEST['security_level'] == "3" ? "selected" : "" ?>>Supreme Leader</option>
+                  </select>
 								</div>
 							</div>
 						</div>
@@ -111,7 +116,7 @@ $mysqli->close();
 							<div class="col-sm-6">
 								<div class="form-group">
 									<div>
-										<button type="submit" name="submit" value="search" id="submit" class="btn btn-primary"><i class="fa fa-fw fa-search"></i> Search</button>
+										<button type="submit" name="submit" value="search" id="submit" class="btn btn-primary btn-space"><i class="fa fa-fw fa-search"></i> Search</button>
 										<a href="<?php echo $_SERVER['PHP_SELF'];?>" class="btn btn-danger"><i class="fa fa-fw fa-sync"></i> Clear</a>
 									</div>
 								</div>
@@ -143,51 +148,34 @@ $mysqli->close();
 				    </tr>
 				  </thead>
 				  <tbody>
-						<tr>
-				      <th scope="row">1</th>
-							<td>Mark Smith</td>
-				      <td>@mdo</td>
-							<td>louischen20@gmail.com</td>
-							<td>@mdo</td>
-							<td align="center">
-								<a href="" class="text-warning"><i class="far fa-edit"></i> Edit</a> |
-								<a href="" class="text-danger"><i class="far fa-trash-alt"></i> Delete</a>
-							</td>
-				    </tr>
-				    <tr>
-				      <th scope="row">2</th>
-							<td>Mark Smith</td>
-				      <td>@mdo</td>
-							<td>louischen20@gmail.com</td>
-							<td>@mdo</td>
-							<td align="center">
-								<a href="" class="text-warning"><i class="far fa-edit"></i> Edit</a> |
-								<a href="" class="text-danger"><i class="far fa-trash-alt"></i> Delete</a>
-							</td>
-				    </tr>
-						<tr>
-				      <th scope="row">3</th>
-							<td>Mark Smith</td>
-				      <td>@mdo</td>
-							<td>louischen20@gmail.com</td>
-							<td>@mdo</td>
-							<td align="center">
-								<a href="" class="text-warning"><i class="far fa-edit"></i> Edit</a> |
-								<a href="" class="text-danger"><i class="far fa-trash-alt"></i>< Delete</a>
-							</td>
-				    </tr>
 						<?php $counter = 1;?>
 						<?php while($row = $results->fetch_assoc()) : ?>
 							<tr>
 							<th scope="row"><?php echo $counter ?></th>
 							<?php $counter++;?>
-							<td><?php echo $row["first_name"] + $row["last_name"]?></td>
-							<td>@mdo</td>
-							<td>louischen20@gmail.com</td>
-							<td>@mdo</td>
+							<td><?php echo $row["first_name"] . " " . $row["last_name"]?></td>
+							<td><?php echo $row["username"]?></td>
+							<td><?php echo $row["email"]?></td>
+							<td>
+                <?php
+                  $security = $row["security_level"];
+                  $display_security = "";
+                  if($security == 3){
+                    $display_security = "Supreme Leader";
+                  }
+                  else if($security == 2){
+                    $display_security = "Administrator";
+                  }
+                  else{
+                    $display_security = "User";
+                  }
+
+                  echo $display_security;
+                ?>
+              </td>
 							<td align="center">
 								<a href="" class="text-warning"><i class="far fa-edit"></i> Edit</a> |
-								<a href="" class="text-danger"><i class="far fa-trash-alt"></i>< Delete</a>
+								<a href="" class="text-danger"><i class="far fa-trash-alt"></i> Delete</a>
 							</td>
 						</tr>
 						<?php endwhile; ?>
