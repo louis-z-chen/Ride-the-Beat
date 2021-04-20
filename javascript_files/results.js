db53a6f72684336ba4aaa3ec29aa72f16eac5086
Louis Chen <chenloui@usc.edu>
2021-04-20 02:28:19 -0400

    new results page

---
+++ javascript_files/results.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  $('#toggle-btn').change(function(){
+    var curr = $(this).prop('checked');
+
+    //if true its spotify, if false its database
+    if(curr){
+      console.log("true");
+      $('#spotify-results').css("display","block");
+      $('#database-results').css("display","none");
+    }
+    else{
+      console.log("false");
+      $('#spotify-results').css("display","none");
+      $('#database-results').css("display","block");
+    }
+
+  });
+});
--- pages/results.php
+++ pages/results.php
@@ -88,118 +88,251 @@ $mysqli->close();
           <div class = "column-content">
 
             <div class="center-text">
-              <h2 class="white-text">Results for "<?php echo $search ?>"</h2>
-              <div class="grey-text">Total Search Results: <?php echo $total_count ?></div>
+              <h1 class="white-text">Results for "<?php echo $search ?>"</h1>
+              <input id="toggle-btn" type="checkbox" checked data-toggle="toggle" data-width="150" data-on="<i class='fab fa-spotify'></i> Spotify" data-off="<i class='fas fa-database'></i> Database" data-onstyle="success" data-offstyle="info">
             </div>
             <br>
+            <div id="spotify-results" class="spotify-results">
+              <!--Row 2 -->
+              <div class="content-row picture-row">
+                <div class="row">
+                  <div class="col-md">
+                    <div class="row-column">
+                      <img src="../images/songs/Song1.jpg" class="playlist-pic"/>
+                      <div class="column-title white-text">Do I Wanna Know?</div>
+                      <div class="column-info grey-text">Artic Monkeys</div>
+                    </div>
+                  </div>
+                  <div class="col-md">
+                    <div class="row-column">
+                      <img src="../images/songs/Song2.png" class="playlist-pic"/>
+                      <div class="column-title white-text">Stuck With U (with Justin Bieber)</div>
+                      <div class="column-info grey-text">Ariana Grande, Justin Bieber</div>
+                    </div>
+                  </div>
+                  <div class="col-md">
+                    <div class="row-column">
+                      <img src="../images/songs/Song3.jpg" class="playlist-pic"/>
+                      <div class="column-title white-text">Cigarettes on Patios</div>
+                      <div class="column-info grey-text">BabyJake</div>
+                    </div>
+                  </div>
+                  <div class="col-md">
+                    <div class="row-column">
+                      <img src="../images/songs/Song4.png" class="playlist-pic"/>
+                      <div class="column-title white-text">CITY OF ANGELS</div>
+                      <div class="column-info grey-text">24KGoldn</div>
+                    </div>
+                  </div>
+                  <div class="col-md">
+                    <div class="row-column">
+                      <img src="../images/songs/Song5.jpg" class="playlist-pic"/>
+                      <div class="column-title white-text">Breezeblocks</div>
+                      <div class="column-info grey-text">alt-J</div>
+                    </div>
+                  </div>
 
-            <div class="container">
-              <!--Database Seearch -->
-              <div class="row">
-                <!--Songs Column -->
-                <div class="col-md">
-                  <div class = "center-text">
-                    <div class="white-text"><strong>Songs</strong> <br></div>
-                    <div class="grey-text"><?php echo $song_count ?> song results</div>
-                  </div>
-                  <table class="table table-dark table-hover">
-                    <thead>
-                      <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Song Name</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <?php $song_results_counter = 1;?>
-                      <?php while($row = $results_song->fetch_assoc() ) : ?>
-                        <tr>
-                        <th scope="row"><?php echo $song_results_counter ?></th>
-                        <?php $song_results_counter++;?>
-                        <td> <?php echo $row["name"] ?> </td>
-                      </tr>
-                      <?php endwhile; ?>
-                    </tbody>
-                  </table>
                 </div>
-                <!--Artist Column -->
-                <div class="col-md">
-                  <div class = "center-text">
-                    <div class="white-text"><strong>Artists</strong> <br></div>
-                    <div class="grey-text"><?php echo $artist_count ?> artist results</div>
+              </div> <!-- Close Row 2 -->
+
+              <!--Row 3 -->
+              <div class="content-row picture-row">
+                <div class="row">
+                  <div class="col-md">
+                    <div class="row-column">
+                      <img src="../images/songs/Song6.png" class="playlist-pic"/>
+                      <div class="column-title white-text">Vienna</div>
+                      <div class="column-info grey-text">Billy Joel</div>
+                    </div>
                   </div>
-                  <table class="table table-dark table-hover">
-                    <thead>
-                      <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Artist Name</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <?php $artist_results_counter = 1;?>
-                      <?php while($row = $results_artist->fetch_assoc() ) : ?>
-                        <tr>
-                        <th scope="row"><?php echo $artist_results_counter ?></th>
-                        <?php $artist_results_counter++;?>
-                        <td> <?php echo $row["name"] ?> </td>
-                      </tr>
-                      <?php endwhile; ?>
-                    </tbody>
-                  </table>
+                  <div class="col-md">
+                    <div class="row-column">
+                      <img src="../images/songs/Song7.jpg" class="playlist-pic"/>
+                      <div class="column-title white-text">Sunny</div>
+                      <div class="column-info grey-text">Bobby Hebb</div>
+                    </div>
+                  </div>
+                  <div class="col-md">
+                    <div class="row-column">
+                      <img src="../images/songs/Song8.jpg" class="playlist-pic"/>
+                      <div class="column-title white-text">I.F.L.Y</div>
+                      <div class="column-info grey-text">Bazzi</div>
+                    </div>
+                  </div>
+                  <div class="col-md">
+                    <div class="row-column">
+                      <img src="../images/songs/Song9.jpg" class="playlist-pic"/>
+                      <div class="column-title white-text">Take Care</div>
+                      <div class="column-info grey-text">Beach House</div>
+                    </div>
+                  </div>
+                  <div class="col-md">
+                    <div class="row-column">
+                      <img src="../images/songs/Song10.jpg" class="playlist-pic"/>
+                      <div class="column-title white-text">when the party's over</div>
+                      <div class="column-info grey-text">Billie Eilish</div>
+                    </div>
+                  </div>
                 </div>
+              </div> <!-- Close Row 3 -->
+
+              <!--Row 4 -->
+              <div class="content-row picture-row">
+                <div class="row">
+                  <div class="col-md">
+                    <div class="row-column">
+                      <img src="../images/songs/Song11.png" class="playlist-pic"/>
+                      <div class="column-title white-text">RedBone</div>
+                      <div class="column-info grey-text">Childish Gambino</div>
+                    </div>
+                  </div>
+                  <div class="col-md">
+                    <div class="row-column">
+                      <img src="../images/songs/Song12.jpg" class="playlist-pic"/>
+                      <div class="column-title white-text">Blessed</div>
+                      <div class="column-info grey-text">Daniel Caesar</div>
+                    </div>
+                  </div>
+                  <div class="col-md">
+                    <div class="row-column">
+                      <img src="../images/songs/Song13.jpg" class="playlist-pic"/>
+                      <div class="column-title white-text">Electric Love</div>
+                      <div class="column-info grey-text">BØRNS</div>
+                    </div>
+                  </div>
+                  <div class="col-md">
+                    <div class="row-column">
+                      <img src="../images/songs/Song14.jpg" class="playlist-pic"/>
+                      <div class="column-title white-text">Passionfruit</div>
+                      <div class="column-info grey-text">Drake</div>
+                    </div>
+                  </div>
+                  <div class="col-md">
+                    <div class="row-column">
+                      <img src="../images/songs/Song15.jpg" class="playlist-pic"/>
+                      <div class="column-title white-text">Hotel California - 2013 Remaster</div>
+                      <div class="column-info grey-text">Eagles</div>
+                    </div>
+                  </div>
+                </div>
+              </div> <!-- Close Row 4 -->
+            </div>
+
+            <div id="database-results"class="database-results">
+              <div class="center-text">
+                <div class="grey-text">Total Search Results: <?php echo $total_count ?></div>
               </div>
-              <div class="row">
-                <!--Playlist Column -->
-                <div class="col-md">
-                  <div class = "center-text">
-                    <div class="white-text"><strong>Playlists</strong> <br></div>
-                    <div class="grey-text"><?php echo $playlist_count ?> playlist results</div>
-                  </div>
-                  <table class="table table-dark table-hover">
-                    <thead>
-                      <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Playlist Name</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <?php $playlist_results_counter = 1;?>
-                      <?php while($row = $results_playlist->fetch_assoc() ) : ?>
+
+              <div class="container">
+                <!--Database Seearch -->
+                <div class="row">
+                  <!--Songs Column -->
+                  <div class="col-md">
+                    <div class = "center-text">
+                      <div class="white-text"><strong>Songs</strong> <br></div>
+                      <div class="grey-text"><?php echo $song_count ?> song results</div>
+                    </div>
+                    <table class="table table-dark table-hover">
+                      <thead>
                         <tr>
-                        <th scope="row"><?php echo $playlist_results_counter ?></th>
-                        <?php $playlist_results_counter++;?>
-                        <td> <?php echo $row["name"] ?> </td>
-                      </tr>
-                      <?php endwhile; ?>
-                    </tbody>
-                  </table>
+                          <th scope="col">#</th>
+                          <th scope="col">Song Name</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <?php $song_results_counter = 1;?>
+                        <?php while($row = $results_song->fetch_assoc() ) : ?>
+                          <tr>
+                          <th scope="row"><?php echo $song_results_counter ?></th>
+                          <?php $song_results_counter++;?>
+                          <td> <?php echo $row["name"] ?> </td>
+                        </tr>
+                        <?php endwhile; ?>
+                      </tbody>
+                    </table>
+                  </div>
+                  <!--Artist Column -->
+                  <div class="col-md">
+                    <div class = "center-text">
+                      <div class="white-text"><strong>Artists</strong> <br></div>
+                      <div class="grey-text"><?php echo $artist_count ?> artist results</div>
+                    </div>
+                    <table class="table table-dark table-hover">
+                      <thead>
+                        <tr>
+                          <th scope="col">#</th>
+                          <th scope="col">Artist Name</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <?php $artist_results_counter = 1;?>
+                        <?php while($row = $results_artist->fetch_assoc() ) : ?>
+                          <tr>
+                          <th scope="row"><?php echo $artist_results_counter ?></th>
+                          <?php $artist_results_counter++;?>
+                          <td> <?php echo $row["name"] ?> </td>
+                        </tr>
+                        <?php endwhile; ?>
+                      </tbody>
+                    </table>
+                  </div>
                 </div>
-                <!--User Column -->
-                <div class="col-md">
-                  <div class = "center-text">
-                    <div class="white-text"><strong>Users</strong><br></div>
-                    <div class="grey-text"><?php echo $user_count ?> username results</div>
-                  </div>
-                  <table class="table table-dark table-hover">
-                    <thead>
-                      <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Username</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <?php $user_results_counter = 1;?>
-                      <?php while($row = $results_user->fetch_assoc() ) : ?>
+                <div class="row">
+                  <!--Playlist Column -->
+                  <div class="col-md">
+                    <div class = "center-text">
+                      <div class="white-text"><strong>Playlists</strong> <br></div>
+                      <div class="grey-text"><?php echo $playlist_count ?> playlist results</div>
+                    </div>
+                    <table class="table table-dark table-hover">
+                      <thead>
                         <tr>
-                        <th scope="row"><?php echo $user_results_counter ?></th>
-                        <?php $user_results_counter++;?>
-                        <td> <?php echo $row["username"] ?> </td>
-                      </tr>
-                      <?php endwhile; ?>
-                    </tbody>
-                  </table>
+                          <th scope="col">#</th>
+                          <th scope="col">Playlist Name</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <?php $playlist_results_counter = 1;?>
+                        <?php while($row = $results_playlist->fetch_assoc() ) : ?>
+                          <tr>
+                          <th scope="row"><?php echo $playlist_results_counter ?></th>
+                          <?php $playlist_results_counter++;?>
+                          <td> <?php echo $row["name"] ?> </td>
+                        </tr>
+                        <?php endwhile; ?>
+                      </tbody>
+                    </table>
+                  </div>
+                  <!--User Column -->
+                  <div class="col-md">
+                    <div class = "center-text">
+                      <div class="white-text"><strong>Users</strong><br></div>
+                      <div class="grey-text"><?php echo $user_count ?> username results</div>
+                    </div>
+                    <table class="table table-dark table-hover">
+                      <thead>
+                        <tr>
+                          <th scope="col">#</th>
+                          <th scope="col">Username</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <?php $user_results_counter = 1;?>
+                        <?php while($row = $results_user->fetch_assoc() ) : ?>
+                          <tr>
+                          <th scope="row"><?php echo $user_results_counter ?></th>
+                          <?php $user_results_counter++;?>
+                          <td> <?php echo $row["username"] ?> </td>
+                        </tr>
+                        <?php endwhile; ?>
+                      </tbody>
+                    </table>
+                  </div>
                 </div>
               </div>
             </div>
+
+
 
           </div>
         </div>
@@ -218,6 +351,7 @@ $mysqli->close();
 
 	<?php require "../reusable_code/footer_files.php"; ?>
   <?php require "../reusable_code/lightmode_files.php"; ?>
+  <script src = "../javascript_files/results.js"></script>
 
 </body>
 </html>
