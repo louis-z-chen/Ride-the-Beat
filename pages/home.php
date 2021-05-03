940c1bda58897d4011b868caff08f421af91c50d
Louis Chen <chenloui@usc.edu>
2021-05-03 13:01:57 -0400

    new home start

--- pages/home.php
+++ pages/home.php
@@ -96,140 +96,121 @@ require "../reusable_code/login_logic.php";
 <body>
 	<?php require "../reusable_code/menu.php"; ?>
 
-  <div class = "main-body center">
+	<div class = "main-body center">
+		<div class="column">
+			<div class="column-content">
 
-    <div class="row">
-      <!--Page Content column -->
-      <div class="col-xl-9 main-column-container">
-        <div class = "column">
-          <div class = "column-content">
-            <!--Left box is split into 3 rows -->
+				<!--Row 1 -->
+				<br>
+				<div class="content-row">
+					<div class="left-banner white-text">
+						<h1 class="bannertext">Welcome <?php echo $curr_first ?>!</h1>
+					</div>
+				</div>
 
-            <!--Row 1 -->
-            <div class="content-row">
-              <div class="left-banner white-text">
-                <h5 class="bannertext">//TRENDING</h5>
-                <h5 class="bannertext">Free Spirit</h5>
-                <h5 class="bannertext">- Khalid</h5>
-              </div>
-            </div>
-
-						<div class= "welcome-message">
-							<h2 class= "white-text">Welcome <?php echo $curr_first ?>!</h2>
+				<!--Row 2 -->
+				<div class="content-row picture-row">
+					<h4 class="row-title white-text">Artist Spotlight</h4>
+					<div class="row">
+						<div class="col-md">
+							<div class="row-column">
+								<img src="../images/Jake2.jpg" class="column-pic"/>
+								<div class="column-title white-text">Jake Hope</div>
+								<div class="column-info grey-text">Pop</div>
+							</div>
 						</div>
+						<div class="col-md">
+							<div class="row-column">
+								<img src="../images/Jake2.jpg" class="column-pic"/>
+								<div class="column-title white-text">Jake Hope</div>
+								<div class="column-info grey-text">Pop</div>
+							</div>
+						</div>
+						<div class="col-md">
+							<div class="row-column">
+								<img src="../images/Jake2.jpg" class="column-pic"/>
+								<div class="column-title white-text">Jake Hope</div>
+								<div class="column-info grey-text">Pop</div>
+							</div>
+						</div>
+						<div class="col-md">
+							<div class="row-column">
+								<img src="../images/Jake2.jpg" class="column-pic"/>
+								<div class="column-title white-text">Jake Hope</div>
+								<div class="column-info grey-text">Pop</div>
+							</div>
+						</div>
+						<div class="col-md">
+							<div class="row-column">
+								<img src="../images/Jake2.jpg" class="column-pic"/>
+								<div class="column-title white-text">Jake Hope</div>
+								<div class="column-info grey-text">Pop</div>
+							</div>
+						</div>
+						<div class="col-md">
+							<div class="row-column">
+								<img src="../images/Jake2.jpg" class="column-pic"/>
+								<div class="column-title white-text">Jake Hope</div>
+								<div class="column-info grey-text">Pop</div>
+							</div>
+						</div>
+					</div>
 
-            <!--Row 2 -->
-            <div class="content-row picture-row">
-              <h4 class="row-title white-text">Artist Spotlight</h4>
-              <div class="row">
-                <div class="col-md">
-                  <div class="row-column">
-                    <img src="../images/Jake2.jpg" class="column-pic"/>
-                    <div class="column-title white-text">Jake Hope</div>
-                    <div class="column-info grey-text">Pop</div>
-                  </div>
-                </div>
-                <div class="col-md">
-									<div class="row-column">
-                    <img src="../images/Jake2.jpg" class="column-pic"/>
-                    <div class="column-title white-text">Jake Hope</div>
-                    <div class="column-info grey-text">Pop</div>
-                  </div>
-                </div>
-                <div class="col-md">
-									<div class="row-column">
-                    <img src="../images/Jake2.jpg" class="column-pic"/>
-                    <div class="column-title white-text">Jake Hope</div>
-                    <div class="column-info grey-text">Pop</div>
-                  </div>
-                </div>
-                <div class="col-md">
-									<div class="row-column">
-                    <img src="../images/Jake2.jpg" class="column-pic"/>
-                    <div class="column-title white-text">Jake Hope</div>
-                    <div class="column-info grey-text">Pop</div>
-                  </div>
-                </div>
-                <div class="col-md">
-									<div class="row-column">
-                    <img src="../images/Jake2.jpg" class="column-pic"/>
-                    <div class="column-title white-text">Jake Hope</div>
-                    <div class="column-info grey-text">Pop</div>
-                  </div>
-                </div>
-                <div class="col-md">
-									<div class="row-column">
-                    <img src="../images/Jake2.jpg" class="column-pic"/>
-                    <div class="column-title white-text">Jake Hope</div>
-                    <div class="column-info grey-text">Pop</div>
-                  </div>
-                </div>
-              </div>
+				</div>
 
-            </div>
+				<!--Row 3 -->
+				<div class="content-row picture-row">
+					<h4 class="row-title white-text">Artist Spotlight</h4>
+					<div class="row">
+						<div class="col-md">
+							<div class="row-column">
+								<img src="../images/Jake2.jpg" class="column-pic"/>
+								<div class="column-title white-text">Jake Hope</div>
+								<div class="column-info grey-text">Pop</div>
+							</div>
+						</div>
+						<div class="col-md">
+							<div class="row-column">
+								<img src="../images/Jake2.jpg" class="column-pic"/>
+								<div class="column-title white-text">Jake Hope</div>
+								<div class="column-info grey-text">Pop</div>
+							</div>
+						</div>
+						<div class="col-md">
+							<div class="row-column">
+								<img src="../images/Jake2.jpg" class="column-pic"/>
+								<div class="column-title white-text">Jake Hope</div>
+								<div class="column-info grey-text">Pop</div>
+							</div>
+						</div>
+						<div class="col-md">
+							<div class="row-column">
+								<img src="../images/Jake2.jpg" class="column-pic"/>
+								<div class="column-title white-text">Jake Hope</div>
+								<div class="column-info grey-text">Pop</div>
+							</div>
+						</div>
+						<div class="col-md">
+							<div class="row-column">
+								<img src="../images/Jake2.jpg" class="column-pic"/>
+								<div class="column-title white-text">Jake Hope</div>
+								<div class="column-info grey-text">Pop</div>
+							</div>
+						</div>
+						<div class="col-md">
+							<div class="row-column">
+								<img src="../images/Jake2.jpg" class="column-pic"/>
+								<div class="column-title white-text">Jake Hope</div>
+								<div class="column-info grey-text">Pop</div>
+							</div>
+						</div>
+					</div>
+				</div>
 
-            <!--Row 3 -->
-						<div class="content-row picture-row">
-              <h4 class="row-title white-text">Artist Spotlight</h4>
-              <div class="row">
-                <div class="col-md">
-                  <div class="row-column">
-                    <img src="../images/Jake2.jpg" class="column-pic"/>
-                    <div class="column-title white-text">Jake Hope</div>
-                    <div class="column-info grey-text">Pop</div>
-                  </div>
-                </div>
-                <div class="col-md">
-									<div class="row-column">
-                    <img src="../images/Jake2.jpg" class="column-pic"/>
-                    <div class="column-title white-text">Jake Hope</div>
-                    <div class="column-info grey-text">Pop</div>
-                  </div>
-                </div>
-                <div class="col-md">
-									<div class="row-column">
-                    <img src="../images/Jake2.jpg" class="column-pic"/>
-                    <div class="column-title white-text">Jake Hope</div>
-                    <div class="column-info grey-text">Pop</div>
-                  </div>
-                </div>
-                <div class="col-md">
-									<div class="row-column">
-                    <img src="../images/Jake2.jpg" class="column-pic"/>
-                    <div class="column-title white-text">Jake Hope</div>
-                    <div class="column-info grey-text">Pop</div>
-                  </div>
-                </div>
-                <div class="col-md">
-									<div class="row-column">
-                    <img src="../images/Jake2.jpg" class="column-pic"/>
-                    <div class="column-title white-text">Jake Hope</div>
-                    <div class="column-info grey-text">Pop</div>
-                  </div>
-                </div>
-                <div class="col-md">
-									<div class="row-column">
-                    <img src="../images/Jake2.jpg" class="column-pic"/>
-                    <div class="column-title white-text">Jake Hope</div>
-                    <div class="column-info grey-text">Pop</div>
-                  </div>
-                </div>
-              </div>
-            </div>
+				<br>
 
-          </div>
-        </div>
-      </div>
-      <!--Music player and lyrics column -->
-      <div class="col-xl-3 main-column-container">
-        <div class = "column">
-          <div class = "column-content">
-            <?php require "../reusable_code/music_player.php";?>
-          </div>
-        </div>
-      </div>
-    </div>
-
+			</div>
+		</div>
 	</div>
 
 	<?php require "../reusable_code/footer_files.php"; ?>
